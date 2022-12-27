0ae658b3a767e68596441211ea82c67d5fb07238
Justin Richer <justin@bspk.io>
2022-12-27 16:23:06 -0500

    zero-index month

--- src/components/vuln.js
+++ src/components/vuln.js
@@ -1,6 +1,7 @@
 const vuln = {
   url: "https://cve.mitre.org/cgi-bin/cvename.cgi?name=2022-23539",
-  date: new Date(2022, 12, 22)
+  // Months are zero-indexed, 0=jan, 1=feb, 11=dec
+  date: new Date(2022, 11, 22)
 };
 
 export default vuln
