a38c41680442ae06e170870f8ceac25844143e9c
Justin Richer <justin@bspk.io>
2023-08-22 10:22:59 +0100

    update to MS token forgery attack

--- src/components/vuln.js
+++ src/components/vuln.js
@@ -1,7 +1,7 @@
 const vuln = {
-  url: "https://cve.mitre.org/cgi-bin/cvename.cgi?name=2022-23539",
+  url: "https://www.microsoft.com/en-us/security/blog/2023/07/14/analysis-of-storm-0558-techniques-for-unauthorized-email-access/",
   // Months are zero-indexed, 0=jan, 1=feb, 11=dec
-  date: new Date(2022, 11, 22)
+  date: new Date(2022, 6, 14)
 };
 
 export default vuln
