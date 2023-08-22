cf06363cb435a8a76faaccb574fd37670b1c4a63
Justin Richer <justin@bspk.io>
2023-08-22 10:23:30 +0100

    fix date

--- src/components/vuln.js
+++ src/components/vuln.js
@@ -1,7 +1,7 @@
 const vuln = {
   url: "https://www.microsoft.com/en-us/security/blog/2023/07/14/analysis-of-storm-0558-techniques-for-unauthorized-email-access/",
   // Months are zero-indexed, 0=jan, 1=feb, 11=dec
-  date: new Date(2022, 6, 14)
+  date: new Date(2023, 6, 14)
 };
 
 export default vuln
